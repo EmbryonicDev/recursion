0f9ed2a953003b273b53a4597d45ff55f818300a
EmbryonicDev <embryonic.dev@gmail.com>
2022-09-14 17:29:50 +0200

    Create fibsRec function to get fibonacci numbers recursively

---
+++ assignment-1/fibs-recursive.js
@@ -0,0 +1,9 @@
+function fibsRec(num) {
+  if (num <= 0) return 'Must be greater than 0';
+  if (num <= 1) return [0];
+  if (num <= 2) return [0, 1];
+
+  return [...fibsRec(num - 1), fibsRec(num - 1)[num - 2] + fibsRec(num - 1)[num - 3]];
+}
+
+console.log(fibsRec(8))
